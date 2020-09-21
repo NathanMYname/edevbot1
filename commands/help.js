aa628ee7daf4c6a922b9330e82f0c1da26b2a188
Nate Zama <imnathanzama@gmail.com>
2020-09-21 11:54:17 -0500

    Tes

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,18 @@ module.exports = {
     execute(message, args) {
         const eEmbed = new Discord.RichEmbed()
         .setColor('BLUE')
-        .setTitle(`Here is some help!`);
+        .setTitle(`Here is some help, Here are all the commands`)
+        .addField('\`!avatar (user)\`, (shows the persons avatar)')
+        .addField('\`!ban (user)\`, admin only')
+        .addField('\`!hi\`, replys with wassup my g')
+        .addField('\`!kick, (user)\`, mods only')
+        .addField('\`!mc\`, shows the membercount for the server.')
+        .addField('\`!mute (user) (time)\`, admin only (mutes a person for a set of time.)')
+        .addField('\`!ping\`, shows the bots ping.')
+        .addField('\`!purge\`, deletes messages (2 - 100)')
+        .addField('\`!rule(1 - 5)\`, shows the rules')
+        .addField('c')
+
         
         message.channel.send(eEmbed);
     }
--- commands/vote.js
+++ commands/vote.js
@@ -1,27 +1,64 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'vote',
+    async execute(message, args) {
+        message.channel.send('Would you like to create a vote?');
 
 
-//[const Discord = require('discord.js');
+        const filter = m => m.author.id === message.author.id;
 
-//module.exports = {
-    //name: 'vote',
-    //async execute(message, args) {
-        //message.channel.send('Would you like to create a vote?');
+        try {
+            let msg = await message.channel.awaitMessages(filter, {
+                maxMatches: 1,
+                time: '15000',
+                error: ['time']
+            })
+            if (msg.content.first() === 'yes') {
+                message.channel.send('What would you like to make the vote about?');
+            } try {
+                let msg = await message.channel.awaitMessages(filter, {
+                    maxMatches: 1,
+                    time: '15000',
+                    error: ['time'],
+                })
+                let embedTopic = msg.content.first();
+                message.channel.send('Enter your first point?');
+                try {
+                    let msg = await message.channel.awaitMessages(filter, {
+                        maxMatches: 1,
+                        time: '15000',
+                        error: ['time'],
+                    })
+                    let firstTopic = msg.content.first();
+                    message.channel.send('Enter your second point');
+                    try {
+                        let msg = await message.channel.awaitMessages(filter, {
+                            maxMatches: 1,
+                            time: '15000',
+                            error: ['time'],
+                        })
+                        let secondTopic = msg.content.first();
+                        const voteEmbed = new Discord.RichEmbed()
+                    .setTitle(embedTopic)
+                    .addField(embedTopic, firstTopic)
+                    .addField(embedTopic, secondTopic).then(msg => msg.react(`\:white_check_mark:`)).then(reaction => reaction.message.react(`\:x:`)).catch(err => console.log(err));
+                    } catch (ERROR) {
+                        message.channel.send('You did not respond fast enough.');
+                        console.log(ERROR);
+                    }
+                } catch (error) {
+                    message.channel.send('You did not respond fast enough.');
+                    console.log(ERROR);
+                }
 
-
-        //const filter = m => m.author.id === message.author.id;
-
-        //try {
-            //let msg = await message.channel.awaitMessages(filter, {
-                //maxMatches: 1,
-                //time: '15000',
-                //error: ['time']
-            //})
-            //if (msg.content.first() === 'yes') {
-                //essage.channel.send('What would you like to make the vote about?')
-            //}
-        //} catch (ERROR) {
-            //message.channel.send('You did not respond fast enough.')
-            //console.log(ERROR);
-        //}
-    //}
-//}
+            } catch {
+                message.channel.send('You did not respond fast enough.');
+                console.log(ERROR);
+            }
+        } catch (ERROR) {
+            message.channel.send('You did not respond fast enough.');
+            console.log(ERROR);
+        }
+    }
+}
