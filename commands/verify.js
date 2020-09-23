cbc5c7478baaeb1e108affea35c6b69a9e1e244c
Nate Zama <imnathanzama@gmail.com>
2020-09-23 10:32:33 -0500

    Tes

--- commands/verify.js
+++ commands/verify.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'verify',
     async execute(message, args) {
-        var verify = false
+        const verify = false
         const role = message.guild.roles.get('756638307307487271');
         const role2 = message.guild.roles.get('756638236188737597');
         if (!role) return message.channel.send('Verify role does not exist.');
@@ -10,11 +10,12 @@ module.exports = {
             message.member.removeRole(role2.id);
             try {
                 verify = false
-                if (verify = true) {
-                    message.channel.send('You have alreayd verified.');
+                if (message.member.hasRole(role)) {
+                    message.member.send('You already verified lol');
                 } else {
                     await message.member.send(`You have verified in ${message.guild.name}`);
-                }
+            }
+                
                 
             } catch (err) {
                 console.log(err);
