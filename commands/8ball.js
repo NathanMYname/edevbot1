97ca220517f14b00061da779cd5340156985ae48
Nate Zama <imnathanzama@gmail.com>
2020-10-16 14:51:16 -0500

    8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: '8ball',
     execute(message, args) {
-        var test = args[0]
-        if (test) return test
+        var test = args[0];
+        if (test) return test;
     }
 }
